a44c1cd37f047f73aa4f22f8ec22356f259c6e7a
Yashi Sahu <yashisahu709@gmail.com>
2024-03-26 02:56:18 +0530

    contact edit

--- script.js
+++ script.js
@@ -15,7 +15,43 @@ panels.forEach(panel => panel.addEventListener('transitionend',togglesActive));
 
 
 
+document.getElementById("name").addEventListener("click", function() {
+    document.getElementById("name-group").classList.add("slideIn");
+  });
+  
+  document.getElementById("email").addEventListener("click", function() {
+    document.getElementById("email-group").classList.add("slideIn");
+  });
+  
+  document.getElementById("message").addEventListener("click", function() {
+    document.getElementById("message-group").classList.add("slideIn");
+    document.getElementById("submit").classList.add("slideIn");
+  });
 
 
+
+
+
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    
+    // You can add validation or other handling here
+    
+    // For demonstration purposes, just displaying the submitted data
+    var status = document.getElementById("status");
+    status.innerHTML = "Name: " + name + "<br>Email: " + email + "<br>Message: " + message;
+  });
+
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".social-icons").style.opacity = "1";
+  });
+  
 
 
